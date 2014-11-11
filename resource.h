3a23e8521181c307b21b8e885fa395468f38950e
chaosworks <maestrochaos@gmail.com>
2014-11-11 15:59:33 +0900

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by gMapViewer.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_GMAPVIEWER_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDI_ICON_REFRESH                130
+#define IDI_ICON_OPEN                   131
+#define IDI_ICON_CONFIG                 147
+#define IDC_EDIT_URL                    1001
+#define IDC_EXPLORER1                   1003
+#define IDC_BUTTON2                     1004
+#define IDC_STATIC_OUTPUT               1005
+#define IDC_STATIC_VERSION              1007
+#define IDC_EDIT_LENGTH                 1008
+#define IDC_BUTTON_VIEWPOINT            1009
+#define IDC_COMBO_TID                   1010
+#define IDC_COMBO_CID                   1011
+#define IDC_EXPLORER2                   1013
+#define IDC_BUTTON_ROADVIEW             1014
+#define IDC_BUTTON_WAVEVIEW             1015
+#define IDC_NTGRAPHCTRL1                1016
+#define IDC_EDIT_WAVEFILE               1018
+#define IDC_BUTTON_OPEN                 1019
+#define IDC_RADIO_TRACK                 1020
+#define IDC_RADIO_ZOOM                  1021
+#define IDC_RADIO_PAN                   1022
+#define IDC_RADIO_CURSOR                1023
+#define IDC_BUTTON_CONFIG_GRAPH         1024
+#define IDC_CHECK_B                     1025
+#define IDC_CHECK_BF                    1026
+#define IDC_CHECK_M                     1027
+#define IDC_CHECK_MF                    1028
+#define IDC_CHECK_D                     1029
+#define IDC_BUTTON_REDRAW               1030
+#define IDC_STATIC_LOC                  1031
+#define IDC_RADIO_ENONE                 1032
+#define IDC_RADIO_ESHORT                1033
+#define IDC_RADIO_EOPEN                 1034
+#define IDC_EXPLORER3                   1035
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1036
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
